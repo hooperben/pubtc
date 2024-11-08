07ab1dbd9b179af97d7c2b5e9421194ac3b9ea6c
Ben Hooper <ben@labrys.io>
2024-11-08 10:07:10 +0700

    chore: fixed helper and added merkle tree levels test

---
+++ contracts/test/setup.test.ts
@@ -0,0 +1,33 @@
+import MerkleTree from "merkletreejs";
+import { loadPoseidon } from "../helpers";
+
+describe("Generating empty merkle tree", async () => {
+  it.only("merkle tree changes should track", async () => {
+    const poseidon2Hash = await loadPoseidon();
+
+    const emptyNote = poseidon2Hash([BigInt(57_69_240)]).toString();
+    const emptyNotes = Array(32).fill(emptyNote);
+
+    console.log(emptyNote);
+
+    const hasherFn = (input: string) => {
+      const [left, right] = [
+        input.slice(0, input.length / 2) as unknown as Buffer,
+        input.slice(input.length / 2) as unknown as Buffer,
+      ];
+
+      const leftBigInt = BigInt("0x" + left.toString("hex"));
+      const rightBigInt = BigInt("0x" + right.toString("hex"));
+
+      const hash = poseidon2Hash([leftBigInt, rightBigInt]).toString();
+      return hash;
+    };
+
+    const tree = new MerkleTree(emptyNotes, hasherFn, {
+      sort: false,
+      complete: true,
+    });
+
+    console.log(tree.toString());
+  });
+});
