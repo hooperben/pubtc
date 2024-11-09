362b22aec6508049972287c643d91d060b875d00
Ben Hooper <ben@labrys.io>
2024-11-09 10:08:34 +0700

    chore: cleaning

--- contracts/contracts/PUBTCK.sol
+++ contracts/contracts/PUBTCK.sol
@@ -1,9 +1,5 @@
-// SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 import "hardhat/console.sol";
 
