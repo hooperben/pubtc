4739aa6995e572532e4d9bd332941f06291c2e0a
Ben Hooper <ben@labrys.io>
2024-11-20 13:40:00 +1000

    chore: checkpoint for handy hash script

--- contracts/scripts/keccak.ts
+++ contracts/scripts/keccak.ts
@@ -1,14 +1,21 @@
 import { keccak256, toUtf8Bytes } from "ethers";
 
 async function main() {
-  const testerString = "hello world";
-  const bytesRep = toUtf8Bytes(testerString);
+  const value1 = "tester_hash1";
+  const value2 = "tester_hash2";
 
-  const hash = keccak256(bytesRep);
-  const hashArray = Uint8Array.from(Buffer.from(hash.slice(2), "hex"));
+  const hash1Bytes = keccak256(toUtf8Bytes(value1));
+  const hash1Array = Uint8Array.from(Buffer.from(hash1Bytes.slice(2), "hex"));
+  console.log(hash1Array);
+
+  const hash2Bytes = keccak256(toUtf8Bytes(value1));
+  const hash2Array = Uint8Array.from(Buffer.from(hash1Bytes.slice(2), "hex"));
+  console.log(hash2Array);
+
+  const hash = keccak256(Uint8Array.from([...hash1Array, ...hash2Array]));
 
   console.log(hash);
-  console.log(hashArray);
+  console.log(Uint8Array.from(Buffer.from(hash.slice(2), "hex")));
 }
 
 main().catch((error) => {
