839d1699e8ff91c05016b9260506dd08b70038b1
Ben Hooper <ben@labrys.io>
2024-11-26 08:36:17 +1000

    chore: added keccak to scripts

--- bashings/build-circuit.sh
+++ bashings/build-circuit.sh
@@ -4,7 +4,7 @@ circuit=$1
 
 if [ "$circuit" == "poseidon" ] || [ "$circuit" == "keccak" ]; then
     echo "building note verify"
-    cd circuits/$circuit/note_verify_keccak/
+    cd ../circuits/$circuit/note_verify/
     nargo compile
     bb write_vk -b ./target/note_verify.json
     bb contract
